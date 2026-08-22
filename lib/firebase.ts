0d8d8df59aabe54b1ffdd4e5dde3151f57ed63f3
ersaiurs <ersaliasyh@gmail.com>
2026-08-22 15:58:54 +0700

    trigger deploy ersaiurs

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
   apiKey:
     process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
